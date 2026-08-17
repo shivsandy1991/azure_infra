1266c61ad8a9353e598dad650e2f388bd7679582
Shivshankar Rai (Contractor) <shivshankar.rai@sita.aero>
2026-08-17 10:02:28 +0530

    pipeline code updated with secrets

--- Environment/prod/main.tf
+++ Environment/prod/main.tf
@@ -11,14 +11,14 @@ module "resource_group" {
   }
 }
 module "vent" {
-  source = "../../child/vnet"
+  source     = "../../child/vnet"
   depends_on = [module.resource_group]
 
   vnetsandy = {
     vnet1 = {
       name                = "vnetprodsandy"
       resource_group_name = "rgprodsandy"
-        location = "Central India"
+      location            = "Central India"
       address_space       = ["10.32.0.0/16"]
     }
   }
@@ -26,17 +26,17 @@ module "vent" {
 }
 
 module "subnet" {
-  source = "../../child/subnet"
-  depends_on = [ module.resource_group,module.vent ]
+  source     = "../../child/subnet"
+  depends_on = [module.resource_group, module.vent]
   subnetsandy = {
     subnet1 = {
-      subnet_name                 = "frontendsandy"
+      subnet_name          = "frontendsandy"
       resource_group_name  = "rgprodsandy"
       virtual_network_name = "vnetprodsandy"
       address_prefixes     = ["10.32.1.0/24"]
     }
     subnet2 = {
-      subnet_name                 = "backendsandy"
+      subnet_name          = "backendsandy"
       resource_group_name  = "rgprodsandy"
       virtual_network_name = "vnetprodsandy"
       address_prefixes     = ["10.32.2.0/24"]
@@ -50,55 +50,55 @@ module "subnet" {
   }
 }
 
-module "public_ip"{
-    source = "../../child/public_ip"
-    depends_on = [ module.resource_group]
-    pip={
-    pip1={
-        pipname                = "pipfrontendsandy"
-  resource_group_name = "rgprodsandy"
-  location = "Central India"
+module "public_ip" {
+  source     = "../../child/public_ip"
+  depends_on = [module.resource_group]
+  pip = {
+    pip1 = {
+      pipname             = "pipfrontendsandy"
+      resource_group_name = "rgprodsandy"
+      location            = "Central India"
 
     }
-    pip2={
-                pipname                = "pipbackendsandy"
-  resource_group_name = "rgprodsandy"
-  location = "Central India"
+    pip2 = {
+      pipname             = "pipbackendsandy"
+      resource_group_name = "rgprodsandy"
+      location            = "Central India"
 
     }
 
+  }
 }
-}
 
-module "vm"{
-    source = "../../child/virtual_machine"
-    depends_on = [ module.vent,module.subnet ]
-    vms = {
-    vm1={
-          vm_name                = "vmprodsandy1"
-  resource_group_name = "rgprodsandy"
-  location = "Central India"
-  nic_name="nicfrontendsandy"
-  virtual_network_name = "vnetprodsandy"
-     subnet_name                 = "frontendsandy"
-     pipname                = "pipfrontendsandy"
-     size="Standard_B1s"
-    #  Standard_D4_v5
-# Standard_D2_V3
+module "vm" {
+  source     = "../../child/virtual_machine"
+  depends_on = [module.vent, module.subnet]
+  vms = {
+    vm1 = {
+      vm_name              = "vmprodsandy1"
+      resource_group_name  = "rgprodsandy"
+      location             = "Central India"
+      nic_name             = "nicfrontendsandy"
+      virtual_network_name = "vnetprodsandy"
+      subnet_name          = "frontendsandy"
+      pipname              = "pipfrontendsandy"
+      size                 = "Standard_B1s"
+      #  Standard_D4_v5
+      # Standard_D2_V3
 
 
-  
+
     }
-    vm2={
-        vm_name                = "vmprodsandy2"
-  resource_group_name = "rgprodsandy"
-  location = "Central India"
-    nic_name="nicbackendsandy"
-  virtual_network_name = "vnetprodsandy"
-     subnet_name                 = "backendsandy"
-     pipname                = "pipbackendsandy"
-  size="Standard_B1s"
+    vm2 = {
+      vm_name              = "vmprodsandy2"
+      resource_group_name  = "rgprodsandy"
+      location             = "Central India"
+      nic_name             = "nicbackendsandy"
+      virtual_network_name = "vnetprodsandy"
+      subnet_name          = "backendsandy"
+      pipname              = "pipbackendsandy"
+      size                 = "Standard_B1s"
     }
-}
+  }
 }
 
